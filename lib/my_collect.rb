ef480579129149a6d581231aaac1ebcffb3f095a
Eric Chrobak <eric@ericchrobak.com>
2018-05-02 00:37:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i += 1
+  end
+  collection
+end
 
-
+array = []
+my_collect(array) do |name|
+  name.split(" ").first
+end
+collection = []
+my_collect(collection) do |lang|
+  lang.upcase
+end
